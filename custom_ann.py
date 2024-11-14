cffb4bf41c110cc36c33fa7114a963202890abfb
Ambaks <78621642+Ambaks@users.noreply.github.com>
2024-11-13 19:56:04 -0500

    Update custom_ann.py

--- custom_ann.py
+++ custom_ann.py
@@ -28,6 +28,10 @@ def ReLU(Z):
     # If using another acticvation func such as ELU, must specify alpha (learning rate) in params.
     return np.maximum(0, Z)
 
+def ELU(Z, alpha=1.0):
+    return np.where(Z > 0, Z, alpha * (np.exp(Z) - 1))
+
+
 def softmax(Z):
         # The activation function applied to the second hidden layer to get the output.
         e = np.exp(Z - Z.max(axis=0, keepdims=True))
@@ -35,7 +39,7 @@ def softmax(Z):
 
 def forward_prop(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
-    A1 = ReLU(Z1)
+    A1 = ELU(Z1)
     Z2 = W2.dot(A1) + b2
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
@@ -49,6 +53,9 @@ def one_hot(Y):
 def deriv_ReLU(Z):
     return Z > 0
 
+def deriv_ELU(Z, alpha=1.0):
+    return np.where(Z > 0, 1, alpha * np.exp(Z))
+
 
 def back_prop(Z1, A1, Z2, A2, W2, X, Y):
     m = Y.size
@@ -56,7 +63,7 @@ def back_prop(Z1, A1, Z2, A2, W2, X, Y):
     dZ2 = A2 - one_hot_Y
     dW2 = 1 / m * dZ2.dot(A1.T)
     db2 = 1 / m * np.sum(dZ2, 1, keepdims= True)
-    dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    dZ1 = W2.T.dot(dZ2) * deriv_ELU(Z1)
     dW1 = 1 / m * dZ1.dot(X.T)
     db1 = 1 / m * np.sum(dZ1, 1, keepdims= True)
     return dW1, db1, dW2, db2
@@ -84,7 +91,9 @@ def gradient_descent(X, Y, iterations, alpha):
             print(f'Iteration: {i}')
             print(f'Accuracy: {round(get_accuracy(get_predictions(A2), Y)*100, 2)}%\n')
     return W1, b1, W2, b2
-iteration = 200
+
+
+iteration = 2000
 start = time.time()
 W1, b1, W2, b2 = gradient_descent(X_train, Y_train, iteration, 0.1)
 finish = time.time()
